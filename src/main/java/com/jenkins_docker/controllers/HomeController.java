06198e7d986dbb528b44708b9b3e24fe79b5472d
Ruben Benito Martinez <ruben2cc@gmail.com>
2026-06-16 18:47:04 -0600

    Se inicia proyecto

---
+++ src/main/java/com/jenkins_docker/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.jenkins_docker.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("hello")
+    public String helloController() {
+        return "Hola Ruben";
+    }
+}
